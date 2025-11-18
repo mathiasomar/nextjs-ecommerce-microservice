373b89128f5dd61b2235dd68eefd7186b467c8b1
mathiasomar <mathiasomar@gmail.com>
2025-11-18 11:13:26 +0300

    Hono.js Auth Middleware

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { shouldBeUser } from "./middleware/authMiddleware.js";
 
 const app = new Hono();
 
@@ -14,17 +15,10 @@ app.get("/health", (c) => {
   });
 });
 
-app.get("/test", (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      message: "You are not logged in.",
-    });
-  }
-
+app.get("/test", shouldBeUser, (c) => {
   return c.json({
     message: "Payment service is authenticated!",
+    userId: c.get("userId"),
   });
 });
 
