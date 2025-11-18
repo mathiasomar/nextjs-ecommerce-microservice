c7ac27bc652c7951ae721f21834705b8297abc6c
mathiasomar <mathiasomar@gmail.com>
2025-11-18 10:50:59 +0300

    Fastify.js microservice authentication

--- apps/client/app/test/page.tsx
+++ apps/client/app/test/page.tsx
@@ -3,14 +3,22 @@ import { auth } from "@clerk/nextjs/server";
 const TestPage = async () => {
   const { getToken } = await auth();
   const token = await getToken();
-  const res = await fetch("http://localhost:8000/test", {
+  const resProduct = await fetch("http://localhost:8000/test", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
-  const data = await res.json();
+  const dataProduct = await resProduct.json();
 
-  console.log(data);
+  const resOrder = await fetch("http://localhost:8001/test", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataOrder = await resOrder.json();
+
+  console.log(dataProduct);
+  console.log(dataOrder);
   return <div>TestPage</div>;
 };
 
