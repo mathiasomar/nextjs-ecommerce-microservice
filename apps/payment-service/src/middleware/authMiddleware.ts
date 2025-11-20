f4519c51d25946a0becc9566a00a6bfe9134a08b
mathiasomar <mathiasomar@gmail.com>
2025-11-20 18:10:57 +0300

    Create role based middleware in payment service

--- apps/payment-service/src/middleware/authMiddleware.ts
+++ apps/payment-service/src/middleware/authMiddleware.ts
@@ -1,6 +1,7 @@
 import { getAuth } from "@hono/clerk-auth";
 import { createMiddleware } from "hono/factory";
 import { Variables } from "hono/types";
+import type { CustomJWTSessionClaims } from "@repo/types";
 
 export const shouldBeUser = createMiddleware<{
   Variables: {
@@ -13,6 +14,30 @@ export const shouldBeUser = createMiddleware<{
     return c.json({
       message: "You are not logged in.",
     });
+  }
+
+  c.set("userId", auth.userId);
+
+  await next();
+});
+
+export const shouldBeAdmin = createMiddleware<{
+  Variables: {
+    userId: string;
+  };
+}>(async (c, next) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({
+      message: "You are not logged in.",
+    });
+  }
+
+  const claims = auth.sessionClaims as CustomJWTSessionClaims;
+
+  if (claims.metadata?.role !== "admin") {
+    return c.json({ message: "you are not an admin" });
   }
 
   c.set("userId", auth.userId);
